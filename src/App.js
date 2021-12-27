cd22a7fbe42314715e45bb044e0ecf4588c3ae95
Alex <alex.smotritsky@gmail.com>
2021-12-27 16:28:46 -0500

    bootstrap import

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Formik, Form, Field, FieldArray, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
     const initialValues = {
